494bdb7f9b67a0ba8c33cd0701c01aec565a59fd
Roberto Francisco da Silva <fs.beto@gmail.com>
2022-12-28 16:48:44 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/fsbeto/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.fsbeto.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fsbeto.workshopmongo.domain.User;
+import com.fsbeto.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+
+}
