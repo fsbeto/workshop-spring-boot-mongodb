4d729acddf6d210c663730feb7f16fce43153ea1
Roberto Francisco da Silva <fs.beto@gmail.com>
2022-12-29 19:13:05 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/fsbeto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fsbeto/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.fsbeto.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fsbeto.workshopmongo.domain.User;
 import com.fsbeto.workshopmongo.dto.UserDTO;
@@ -21,8 +24,8 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method = RequestMethod.GET)
 	//forma alternativa @GetMapping
+	@RequestMapping(method = RequestMethod.GET)	
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
@@ -33,5 +36,20 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {		
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	//forma alternativa @PostMapping
+	@RequestMapping(method = RequestMethod.POST)	
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {		
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();		
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)	
+	public ResponseEntity<Void> delete(@PathVariable String id) {		
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/fsbeto/workshopmongo/services/UserService.java
+++ src/main/java/com/fsbeto/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fsbeto.workshopmongo.domain.User;
+import com.fsbeto.workshopmongo.dto.UserDTO;
 import com.fsbeto.workshopmongo.repository.UserRepository;
 import com.fsbeto.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,18 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User fromDTO(UserDTO objtDto) {
+		return new User(objtDto.getId(), objtDto.getName(), objtDto.getEmail());
 	}
 }
