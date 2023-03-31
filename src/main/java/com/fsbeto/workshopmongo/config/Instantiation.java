ab61fabcd652be5d80f7a86eee242dc5a6de3b42
Roberto Francisco da Silva <fs.beto@gmail.com>
2023-03-31 17:56:26 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/fsbeto/workshopmongo/config/Instantiation.java
+++ src/main/java/com/fsbeto/workshopmongo/config/Instantiation.java
@@ -22,11 +22,9 @@ public class Instantiation implements CommandLineRunner {
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		User graciana = new User(null, "Graciana Amor", "gra@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");		
 		
-		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob, graciana));
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 	}
 
 }
